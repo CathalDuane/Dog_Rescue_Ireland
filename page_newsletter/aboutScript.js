c910e51ff1297c545ab4f84a45e08ced812d0448
CathalDuane <duane.cathal@gmail.com>
2021-12-14 23:13:58 +0100

    CD: mostly tidied up newsletter page

--- page_newsletter/aboutScript.js
+++ page_newsletter/aboutScript.js
@@ -7,8 +7,6 @@ function monthselect() {
 
 //Cathal: jQuery slider
 jQuery(document).ready(function ($) {
-
-   
     
       var slideCount = $('#slider ul li').length;
       var slideWidth = $('#slider ul li').width();
@@ -46,8 +44,8 @@ jQuery(document).ready(function ($) {
       $('a.control_next').click(function () {
           moveRight();
       });
-  
-  });  
+  }); 
+ 
       
 //Cathal: jQuery email valiadation
 $(document).ready(function() { 
@@ -70,9 +68,69 @@ $(document).ready(function() {
         $("#UserEmail").after('<span class="error">Enter a valid email address.</span>');
         hasError = true;
       }
-   
-   
+
       if(hasError == true) { return false; }
-   
       });
   });
+
+//Cathal: True/False Game
+//simple true false game with JavaScript
+let score = 0;
+let attempts = 0;
+function question1(){
+    let userInput = document.getElementById("userAns1").value;
+    if (userInput === 'T'|| userInput === 't'){
+        score += 1;
+        attempts += 1;
+       document.getElementById("question1").innerHTML = "Q1 answered";
+    }
+    else if(userInput === 'F'|| userInput === 'f'){  
+        attempts += 1;
+        document.getElementById("question1").innerHTML = "Q1 answered";
+    }  
+} 
+
+function question2(){
+    let userInput = document.getElementById("userAns2").value;
+    if (userInput === 'T'|| userInput === 't'){
+        attempts += 1;
+        document.getElementById("question2").innerHTML = "Q2 answered";
+        }
+    else if(userInput === 'F'|| userInput === 'f'){  
+        score += 1;
+        attempts += 1;
+        document.getElementById("question2").innerHTML = "Q2 answered";
+    }
+    
+} 
+
+function question3(){
+    let userInput = document.getElementById("userAns3").value;
+    if (userInput === 'T'|| userInput === 't'){
+        score += 1;
+        attempts += 1;
+        document.getElementById("question3").innerHTML = "Q3 answered";
+        }
+
+    else if(userInput === 'F'|| userInput === 'f'){  
+        document.getElementById("question3").innerHTML = "Q3 answered";
+        attempts += 1;
+    }   
+} 
+
+function getResults(){
+    if(attempts === 3){
+        document.getElementById("questionFormat").innerHTML = "You got "+ score + "out of 3";
+        score = 0;
+    }
+    else{
+        document.getElementById("unanswered").innerHTML += "you must answer all the questions";
+    }
+
+}
+
+
+
+
+
+
