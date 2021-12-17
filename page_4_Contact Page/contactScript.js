9dd67afdb611b135b347b0890c3407a326850523
CathalDuane <duane.cathal@gmail.com>
2021-12-17 17:17:44 +0100

    CD 9

--- page_4_Contact Page/contactScript.js
+++ page_4_Contact Page/contactScript.js
@@ -1,23 +1,32 @@
-
-
-//Cathal: Function to change HTML to show user input was succesful (Does not account for required email validation)
+//Cathal: Function to change HTML to show user input was successful or if more input is required 
 function validateForm() {
-    let x = document.forms["contactForm"]["nameInput"].value;
-    let y = document.forms["contactForm"]["emailInput"].value;
-    if (x == "" || y == "") {
-    }
-    else if (x == "" && y == ""){
-    }
-    else{
-        submitSuccess()
-    }
-  }
 
+    let emailRequired1 = false;
+    let nameValue = document.forms["contactForm"]["nameInput"].value;
+    let emailValue = document.forms["contactForm"]["emailInput"].value;
+
+    // For loop to check if '@' char is included in String user input
+    for (let i = 0; i < emailValue.length; i++){
+        if (emailValue[i] == '@'){
+            emailRequired1 = true;
+        }
+    }
+
+    //If statement to make sure inputs are filled before changing innerHTML for message
+    if(nameValue != "" && emailRequired1 == true && emailValue != ""){
+        //Calling input complete message
+        submitSuccess()
+    }  
+    else if (nameValue == "" || emailValue== "" ) {
+    }
+    else if (nameValue == "" && emailValue == "" ){
+    } 
+}
+
+//function to change innerHTML when input is completed
 function submitSuccess(){
     event.preventDefault;
     document.getElementById("contactInput").innerHTML = "<h2>Thank you for your message </h2><br> We will contact you soon";    
 }
 
 
-
-    
