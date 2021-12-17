a5a232eff7bf652344a0d34cc7680015c6f91bf4
CathalDuane <duane.cathal@gmail.com>
2021-12-18 00:33:34 +0100

    CD12

--- page_4_Contact Page/contactScript.js
+++ page_4_Contact Page/contactScript.js
@@ -1,14 +1,17 @@
 //Cathal: Function to change HTML to show user input was successful or if more input is required 
 function validateForm() {
-
+ 
     let emailRequired1 = false;
-    let nameValue = document.forms["contactForm"]["nameInput"].value;
-    let emailValue = document.forms["contactForm"]["emailInput"].value;
+    let nameValue = document.getElementById('nameInput').value;
+    let emailValue = document.getElementById('emailInput').value;
+
+    //JavaScript input test
+    // alert(nameValue +"\n"+emailValue );
 
     // For loop to check if '@' char is included in String user input
     for (let i = 0; i < emailValue.length; i++){
         if (emailValue[i] == '@'){
-            emailRequired1 = true;
+            emailRequired1 = true;  
         }
     }
 
@@ -17,10 +20,18 @@ function validateForm() {
         //Calling input complete message
         submitSuccess()
     }  
-    else if (nameValue == "" || emailValue== "" ) {
+    if (nameValue == "" ) {
+        document.getElementById('alertMessageName').innerHTML = "Name required";    
+
     }
-    else if (nameValue == "" && emailValue == "" ){
+    if (emailValue == "" ) {
+        document.getElementById('alertMessageEmailEmpty').innerHTML = "Email required"; 
+        
+    }
+    if (emailValue != "" && emailRequired1 == false ){
+        document.getElementById('alertMessageEmailEmpty').innerHTML = "@ required";
     } 
+   
 }
 
 //function to change innerHTML when input is completed
